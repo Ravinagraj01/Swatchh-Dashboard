0a8ec021236b62cead8ace464d55c925f1feacf9
Ravinagraj01 <ravi.nagraj01@gmail.com>
2025-03-21 18:44:28 +0530

    user model

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,54 @@
+import generateToken from "../config/generateToken";
+import User from "../models/User";
+
+
+
+//User RegistrationL
+export const registerUser = async (req, res) => {
+    const {name, email, password} = req.body;
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        return res.status(400).json({
+            status: 0,
+            message: "User already exists"
+        })
+    }
+
+    const user = await User.create({name, email, password});
+
+    if(user){
+        return res.status(201).json({
+            status: 1,
+            _id: user._id,
+            name : user.name,
+            email : user.email,
+            token : generateToken(user._id)
+        });
+    }else{
+        return res.status(400).json({
+            status: 0,
+            message: "Invalid user data"
+        })
+    };
+
+}
+
+//Login user:
+export const authuser = async (req, res) => {
+    const {email, password} = req.body;
+    const user = User.findOne({email});
+
+    if(user && (await user.matchPassword(password))){
+        res.json({ 
+            _id: user._id, 
+            name: user.name, 
+            email: user.email, 
+            token: generateToken(user._id) });
+    }else{
+        res.status(401).json({
+            status: 0,
+            message: "Invalid email or password"
+        })
+    };
+};
